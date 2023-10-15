e55b43f3d96efc7275520ff18141f5c966fd7d28
EOEboh <ecolejnr007@gmail.com>
2023-10-15 17:47:03 +0100

    config setup on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      gray: {
+        50: "#f9fafb",
+        600: "#4b5563",
+        700: "#374151",
+        900: "#111827",
+      },
+    },
+    fontSize: {
+      base: "1rem", // 16px
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+    },
+
     extend: {},
   },
   plugins: [],
