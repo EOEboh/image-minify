faab9af3b589eb4d16c655edbd42a4b9c93fa14b
EOEboh <ecolejnr007@gmail.com>
2023-11-20 14:56:36 +0100

    download feature

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
-// Initialization for ES Users
-import { Collapse, Select, Ripple, initTE } from "tw-elements";
-
-initTE({ Collapse, Select, Ripple });
-
 const uploadElement = document.getElementById("uploadEl");
+
+function convertBytesToMegabytes(bytes = 1048576) {
+  // 1 byte is equal to 9.5367431640625e-7 megabytes
+  const megabytes = bytes * 9.5367431640625e-7;
+  return megabytes.toFixed(2);
+}
+
+console.log("test", convertBytesToMegabytes());
 
 uploadElement.addEventListener("change", () => {
   const uploadedFile = uploadElement.files[0];
@@ -16,8 +19,6 @@ uploadElement.addEventListener("change", () => {
   img.src = url;
 
   img.onload = function () {
-    // document.body.appendChild(img);
-
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
 
@@ -27,23 +28,47 @@ uploadElement.addEventListener("change", () => {
     context.drawImage(img, 0, 0);
 
     const compressBtn = document.querySelector("#compress-btn");
+    const compressBtnText = document.querySelector("#cta-btn-text");
+    const compressSize = document.querySelector("#compress-file-size");
 
     compressBtn.addEventListener("click", () => {
+      const mimeType = document.getElementById(
+        "compressing-format-select"
+      ).value;
+      console.log("format", mimeType);
+
       canvas.toBlob(
         (blob) => {
           console.log("compressed", blob.size);
           const fr = new FileReader();
 
-          fr.readAsDataURL(blob);
-
           fr.addEventListener("load", () => {
+            document.querySelector("#compress-figcaption").textContent =
+              "Your compressed image below:";
+
             const dataURL = fr.result;
             const finalImage = document.getElementById("compressed-image");
             finalImage.src = dataURL;
-            console.log("dataURL", dataURL);
+            // console.log("dataURL", dataURL);
+
+            finalImage.onload = () => {
+              compressSize.textContent = `${convertBytesToMegabytes(
+                blob.size
+              )}MB`;
+
+              compressBtnText.innerHTML = `
+              <a href="${dataURL}" download="${uploadedFile.name
+                .split(".")
+                .unshift()}_compressed">
+                <button>Download</button>
+              </a>
+            `;
+            };
           });
+
+          fr.readAsDataURL(blob);
         },
-        "image/webp",
+        mimeType,
         0.9
       );
     });
